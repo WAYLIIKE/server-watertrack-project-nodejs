e0144f5c46b42a0f214b18b3e3fcc4e8035b6216
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2024-05-10 02:37:46 +0200

    update water part

--- helpers/validateBody.js
+++
@@ -1,9 +0,0 @@
-export const validateBody = (schema, data) => {
-  const { error, value } = schema.validate(data);
-
-  if (!error) return { value };
-
-  return {
-    error: error.details.map(err => err.message),
-  };
-};
--- routes/waterRouter.js
+++ routes/waterRouter.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import { createWater } from '../controllers/waterController.js';
+import { joiValidateDataMiddleware } from '../middlewares/joiValidatorMiddleware.js';
+import { createWaterSchema } from '../schemas/waterSchemas.js';
+import { protection } from '../middlewares/usersMiddlewares.js';
 
 const waterRouter = express.Router();
 
 waterRouter.post('/add');
+waterRouter.post(
+  '/add',
+  protection,
+  joiValidateDataMiddleware(createWaterSchema),
+  createWater,
+);
 waterRouter.patch('/edit/:id');
 waterRouter.delete('/remove/:id');
 waterRouter.get('/day/:date');
-waterRouter.get('/month/:date');
-
-export { waterRouter };
