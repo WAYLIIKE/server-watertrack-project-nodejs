ece18d9737dc620dbdef69834a6b1f7f08f77a57
Artemskyba <artemgennadievich@icloud.com>
2024-05-11 23:13:00 +0300

    edit water joi schema

--- schemas/waterSchemas.js
+++ schemas/waterSchemas.js
@@ -1,15 +1,11 @@
 import Joi from 'joi';
 
-export const createWaterSchema = Joi.object()
-  .options({})
-  .keys({
-    date: Joi.date().required(),
-    amount: Joi.number().min(10).max(2000).required(),
-  });
+export const createWaterSchema = Joi.object({
+  date: Joi.date().required(),
+  amount: Joi.number().min(10).max(2000).required(),
+});
 
-export const updateWaterSchema = Joi.object()
-  .options({})
-  .keys({
-    date: Joi.date(),
-    amount: Joi.number().min(10).max(2000).required(),
-  });
+export const updateWaterSchema = Joi.object({
+  date: Joi.date(),
+  amount: Joi.number().min(10).max(2000).required(),
+});
